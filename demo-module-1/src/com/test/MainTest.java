e4ecd4cb0114407bdc36013c4bdbca397bbdc470
${bigboy1208} <703318526@qq.com>
2021-06-13 16:52:28 +0800

    项目自身配置同时提交第一次测试

--- demo-module-1/src/com/test/MainTest.java
+++ demo-module-1/src/com/test/MainTest.java
@@ -4,6 +4,7 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("test chang");
+        System.out.println("test chang");
 
     }
 }
