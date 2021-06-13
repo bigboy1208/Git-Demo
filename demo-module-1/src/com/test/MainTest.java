34429126ee8465b7ca8d76518029312dd199fea1
${bigboy1208} <703318526@qq.com>
2021-06-13 15:51:50 +0800

    尝试第二次更改

--- demo-module-1/src/com/test/MainTest.java
+++ demo-module-1/src/com/test/MainTest.java
@@ -2,6 +2,7 @@ package com.test;
 
 public class MainTest {
     public static void main(String[] args) {
+        System.out.println("hello git");
 
     }
 }
--- demo-module-1/src/com/test/MainTest1.java
+++ demo-module-1/src/com/test/MainTest1.java
@@ -1,4 +1,9 @@
 package com.test;
 
 public class MainTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+
+    }
 }
