4dac6ca4bb32dfe4b3be23c609d83e1098243a6c
${bigboy1208} <703318526@qq.com>
2021-06-13 17:01:50 +0800

    项目自身配置正式提交

--- demo-module-1/src/com/test/MainTest.java
+++ demo-module-1/src/com/test/MainTest.java
@@ -4,6 +4,8 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("test chang");
+        System.out.println("test chang");
+        System.out.println("test chang");
 
     }
 }
